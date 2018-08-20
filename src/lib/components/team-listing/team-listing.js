2d18c8e7df494d2eb9e74c9c659d39769e649c59
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-20 16:47:09 -0400

    Converted Team Listing into a component

--- src/lib/components/team-listing/team-listing.js
+++ src/lib/components/team-listing/team-listing.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./team-listing.scss";
 
-const TeamListing = ({ teamMembers }) => {
-  const team = teamMembers.map(teamMember => {
-    return (
-      <Link
-        className="header-link"
-        to={`/agents/${teamMember._id}`}
-        key={teamMember._id}
-      >
-        <div className="project-team-member-image">
-          <img
-            src={teamMember.image}
-            alt={`${teamMember.firstName} ${teamMember.lastName}`}
-          />
-        </div>
-      </Link>
-    );
-  });
+class TeamListing extends Component {
 
-  return <div className="project-team">{team}</div>;
-};
+  createTeamListing = teamMembers => {
+    const team = teamMembers.map(teamMember => {
+      return (
+        <div>
+          <Link
+            className="header-link"
+            to={`/agents/${teamMember._id}`}
+            key={teamMember._id}
+          >
+            <div className="project-team-member-image">
+              <img
+                src={teamMember.image}
+                alt={`${teamMember.firstName} ${teamMember.lastName}`}
+              />
+            </div>
+          </Link>
+          <p>{`${teamMember.firstName} ${teamMember.lastName}`}</p>
+        </div>
+      );
+    });
+  
+    return <div className="project-team">{team}</div>;
+  };
+
+  render() {
+    const teamListing = this.createTeamListing(this.props.teamMembers);
+    return teamListing;
+  }
+}
+
 
 export default TeamListing;
