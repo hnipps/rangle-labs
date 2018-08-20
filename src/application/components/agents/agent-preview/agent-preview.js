f24e2642dc1ced2040698fcdb1f3a1624c4fc64e
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-20 15:48:34 -0400

    Added technology tags to the agent preview component

--- src/application/components/agents/agent-preview/agent-preview.js
+++ src/application/components/agents/agent-preview/agent-preview.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./agent-preview.scss";
+import TechnologyTag from "../../technologies/technology-tag/technology-tag"
 
 class AgentPreview extends Component {
   render() {
     const { agent } = this.props;
+    
+    const technologies = agent.currentTechnologies.map(tech => {
+      return (<TechnologyTag
+        technology={tech}
+        key={tech._id}
+        isActiveFilter={false}
+        isDisabled={false}
+        handleTechFilter={techId => {return;}}
+      />);
+    });
+
     return (
       <div className="agent-box">
         <Link to={`/agents/${agent._id}`}>
@@ -19,6 +31,8 @@ class AgentPreview extends Component {
             <h4>{agent.role}</h4>
           </div>
         </Link>
+        <br></br>
+        {technologies}
       </div>
     );
   }
