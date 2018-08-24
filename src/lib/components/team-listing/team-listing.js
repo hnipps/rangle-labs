27964fffa33ff788e1d004cadd1baac9e84d7010
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-24 16:10:33 -0400

    Styled the team listing 'add' and 'remove' buttons

--- src/lib/components/team-listing/team-listing.js
+++ src/lib/components/team-listing/team-listing.js
@@ -1,40 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Button from "../button/button";
 
 class TeamListing extends Component {
 
   createTeamListing = teamMembers => {
+    let team;
 
-    const team = teamMembers.map((teamMember, i) => {
-      let addButton;
-      let removeButton;
-      if (this.props.onAddClick) {
-        addButton = <button value={i} onClick={this.props.onAddClick}>+</button>
-      }
-      if (this.props.onRemoveClick) {
-        removeButton = <button value={i} onClick={this.props.onRemoveClick}>-</button>
-      }
-      return (
-        <li className="dib mr1 mb1" key={teamMember._id}>
-          <Link
-            className=""
-            to={`/agents/${teamMember._id}`}
-          >
-            <div>
-              <img className="mw3 br-100"
-                src={teamMember.image}
-                alt={`${teamMember.firstName} ${teamMember.lastName}`}
-              />
-            </div>
-          </Link>
-          <h2 className="mv0 f7 mw3 center">{`${teamMember.firstName} ${teamMember.lastName}`}</h2>
-          {addButton}
-          {removeButton}
-        </li>
-      );
-    });
+    if (teamMembers.length > 0) {
+      team = teamMembers.map((teamMember, i) => {
+        let addButton;
+        let removeButton;
+        if (this.props.onAddClick) {
+          addButton = <Button value={i} color={"green"} onClick={this.props.onAddClick}>Add</Button>
+        }
+        if (this.props.onRemoveClick) {
+          removeButton = <Button value={i} color={"dark-red"} onClick={this.props.onRemoveClick}>Remove</Button>
+        }
+        return (
+          <li className="dib mr1 mb1 tc h4" key={teamMember._id}>
+            <Link
+              className=""
+              to={`/agents/${teamMember._id}`}
+            >
+              <div>
+                <img className="mw3 br-100"
+                  src={teamMember.image}
+                  alt={`${teamMember.firstName} ${teamMember.lastName}`}
+                />
+              </div>
+            </Link>
+            <h2 className="f7 mw3 center mt0 mb2">{`${teamMember.firstName} ${teamMember.lastName}`}</h2>
+            {addButton}
+            {removeButton}
+          </li>
+        );
+      });
+    } else {
+      team = (
+        <div className="dtc v-mid tc moon-gray ph3 ph4-l h-100">
+          <p className="f4 f3-ns fw6 tc">{ this.props.placeholder }</p>
+        </div>
+      )
+    }
+
   
-    return <ul className="list ph2 pv1 ma0">{team}</ul>;
+    return <ul className="list ph2 pv0 ma0 h4 mb2 dt w-100">{team}</ul>;
   };
 
   render() {
