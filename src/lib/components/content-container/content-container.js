dddb36e49c1f4ea3dc3905065c9b6c106edd1f4d
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-08-27 10:23:04 -0400

    Moved technology tags to be above project list

--- src/lib/components/content-container/content-container.js
+++ src/lib/components/content-container/content-container.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class ContentContainer extends Component {
   render() {
-    return <div className="ph2 ph4-ns flex flex-column flex-row-ns">{ this.props.children }</div>;
+    return <div className="ph2 ph4-ns flex flex-column">{ this.props.children }</div>;
   }
 }
 
