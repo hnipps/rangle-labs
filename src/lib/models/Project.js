e485b1a64bad09b84830bfbc1a0562a6e798b5d2
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-09-06 12:26:49 -0400

    Added trello board link Input added to the add-project page, for adding the URL If URL entered, the trello logo is shown on the detail page w/ link

--- src/lib/models/Project.js
+++ src/lib/models/Project.js
@@ -1,35 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    difficulty: {
-        type:  Number,
-        required: false
-    },
-    status: {
-      type: String,
-      required: false,
-    },
-    technologies: [{
-        type: Schema.Types.ObjectId, 
-        ref: 'Technology',
-    }],
-    projectLead: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Agent',
-    }],
-    agents: [{
-        type: Schema.Types.ObjectId, 
-        ref: 'Agent'
-    }],
-})
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  difficulty: {
+    type: Number,
+    required: false
+  },
+  status: {
+    type: String,
+    required: false
+  },
+  technologies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Technology"
+    }
+  ],
+  projectLead: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Agent"
+    }
+  ],
+  agents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Agent"
+    }
+  ],
+  trelloBoardUrl: {
+    type: String,
+    required: false
+  },
+  githubUrl: {
+    type: String,
+    required: false
+  }
+});
 
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model("Project", projectSchema);
