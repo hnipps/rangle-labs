d5f20a5a926bcd48c8b1088a8807a011e49730e5
Nick Yang <nick.yang@rangle.io>
2018-09-11 12:04:27 -0400

    Added Active Agent Filter Button

--- src/application/components/agents/agent-listing/agent-listing.js
+++ src/application/components/agents/agent-listing/agent-listing.js
@@ -7,6 +7,7 @@ import CardContainer from "../../../../lib/components/card-container/card-contai
 import SidebarContainer from "../../../../lib/components/sidebar-container/sidebar-container";
 import ControlContainer from "../../../../lib/components/control-container/control-container";
 import LinkButton from "../../../../lib/components/link-button/link-button";
+import Button from "../../../../lib/components/button/button";
 
 class AgentListing extends Component {
   async componentDidMount() {
@@ -26,7 +27,11 @@ class AgentListing extends Component {
 
   renderAgents = agents => {
     if (!agents.length) {
-      return <h2 className="center tc moon-gray" >Sorry, no agents match your criteria!</h2>;
+      return (
+        <h2 className="center tc moon-gray">
+          Sorry, no agents match your criteria!
+        </h2>
+      );
     }
 
     return agents.map(agent => {
@@ -49,14 +54,15 @@ class AgentListing extends Component {
             agents={this.props.agents}
           />
           <ControlContainer>
-            <LinkButton to="/add-agent" color="dark-red" >
+            <LinkButton to="/add-agent" color="dark-red">
               Add Agent
             </LinkButton>
+            <Button onClick={this.props.toggleActiveAgentFilter} color="green">
+              Toggle Active Agents
+            </Button>
           </ControlContainer>
         </SidebarContainer>
-        <CardContainer>
-          {this.renderAgents(this.props.agents)}
-        </CardContainer>
+        <CardContainer>{this.renderAgents(this.props.agents)}</CardContainer>
       </ContentContainer>
     );
   }
