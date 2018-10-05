d1fa13636ddb5029bdcefb9479d81d21f7b7c0f5
Nick Yang <nick.yang@rangle.io>
2018-10-05 11:38:55 -0400

    :bug: Fixing mentorship view issues

--- src/application/components/mentorships/mentorship-preview/mentorship-preview.js
+++ src/application/components/mentorships/mentorship-preview/mentorship-preview.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ProjectStatus from "../../../../lib/components/status/mentorship-status/mentorship-status";
-import DifficultyPips from "../../../../lib/components/difficulty-pips/difficulty-pips";
 import TeamListing from "../../../../lib/components/team-listing/team-listing";
-import TechListing from "../../../../lib/components/tech-listing/tech-listing";
 import Card from "../../../../lib/components/card/card";
 import CardTitle from "../../../../lib/components/card/card-title/card-title";
 import CardContent from "../../../../lib/components/card/card-content/card-content";
@@ -19,7 +17,6 @@ class MentorshipPreview extends Component {
           <div className="dtc v-mid mid-gray mb0">
             <ProjectStatus status={mentorship.status} />
           </div>
-          <DifficultyPips difficulty={mentorship.difficulty} />
         </CardHeader>
         <CardContent>
           <CardTitle to={`/mentorships/${mentorship._id}`}>{mentorship.title}</CardTitle>
@@ -29,11 +26,8 @@ class MentorshipPreview extends Component {
           >
             <p>{mentorship.description}</p>
           </Link>
-
-          {/* TECH TOOL LISTING */}
-          <TechListing technologies={mentorship.technologies} />
           {/* PROJECT LEAD LISTING */}
-          <TeamListing teamMembers={mentorship.mentorshipLead} />
+          <TeamListing teamMembers={mentorship.mentorshipLead} leadFlag/>
           {/* TEAM LISTING*/}
           <TeamListing teamMembers={mentorship.agents} />
         </CardContent>
