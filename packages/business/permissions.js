c50073be4a91220e1312b2787df3b943a76fdb35
Nick Yang <nick.yang@rangle.io>
2018-12-12 16:48:25 -0500

    Add initial roles and permissions

---
+++ packages/business/permissions.js
@@ -0,0 +1,13 @@
+export const AdminPermissions = {
+  canAddMentorship: true,
+  canRemoveMentorship: true,
+  canViewAllMentorships: true,
+  canEditMentorships: true,
+}
+
+export const UserPermissions = {
+  canAddMentorship: false,
+  canRemoveMentorship: true,
+  canViewAllMentorships: false,
+  canEditMentorships: true,
+}
